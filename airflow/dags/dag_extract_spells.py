a7b7911c94b0d753a8f32258a1c238992c96a90d
Vitor Roque <vitor.roquep@gmail.com>
2024-05-17 14:00:11 -0300

    feat: added docker operators

--- airflow/dags/dag_extract_spells.py
+++ airflow/dags/dag_extract_spells.py
@@ -1,36 +1,11 @@
 import sys
 import os
-print(sys.path)
-sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 from datetime import timedelta
 from airflow.models.dag import DAG
 
-from airflow.operators.python import PythonOperator
-from airflow.decorators import task
-
-import mod.base as api_extractor
-import mod.transform as transformer
-
-@task(task_id="extract_batch_spells")
-def extract_batch_spells():
-    print("extracting spells...")
-    spells = api_extractor.get_spells_from_api()
-    return transformer.turn_into_batches(spells)
-
-@task(task_id="transform_batches")
-def transform_batches(batches):
-    print("transforming...")
-    for batch in batches:
-        proc = Process(target=execute_transformer, args=(batch,))
-        proc.start()
-
-def execute_transformer(batch: transformer.SpellBatch):
-    return transformer.Transformer(batch=batch.spells) \
-            .apply(transformer.transform_description) \
-            .apply(transformer.transform_components) \
-            .apply(transformer.transform_range) \
-            .apply(transformer.transform_damage)
+from airflow.operators.docker_operator import DockerOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 with DAG(
     "spells",
@@ -44,6 +19,18 @@ with DAG(
     },
     description="The Spells Ingestion DAG"
 ) as dag:
-    spells = extract_batch_spells()
-    batches = transform_batches(spells)
-
+    start_dag = DummyOperator(
+        task_id='start_dag'
+    )
+    extract_task = DockerOperator(
+        task_id='extract_spells',
+        image='dungeonsnpipes-spells-extract:latest',
+        container_name='spells-extract',
+        docker_url="unix://var/run/docker.sock",
+        api_version='auto',
+        auto_remove=True,
+        network_mode='bridge'
+    )
+    start_dag >> extract_task
+    # spells = extract_batch_spells()
+    # batches = transform_batches(spells)
