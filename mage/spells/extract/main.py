3cb7b20246a96e3a6e0e8412e492dd13d5b03590
Vitor Roque <vitor.roquep@gmail.com>
2024-05-20 02:08:34 -0300

    feat: adding caching and extraction of spells one by one

--- mage/spells/extract/main.py
+++ mage/spells/extract/main.py
@@ -3,26 +3,30 @@ import json
 from io import BytesIO
 from minio import Minio
 
+client = Minio('minio1:9000', access_key=base.ACCESS_KEY,
+               secret_key=base.SECRET_KEY, secure=False)
 
-def save_object(folder: str, filename: str):
-    client.put_object(folder, filename)
+def save_index_spell(folder:str, filename: str, data: dict):
+    client.put_object(folder, filename, BytesIO(json.dumps(data).encode('utf-8')), length=-1, part_size=10*1024*1024)
+
+def get_index_spell(spell: str):
+    client.get_object("spells", f'indexes/{spell}.json')
 
 
 def main():
-    client = Minio('minio1:9000', access_key=base.ACCESS_KEY,
-                   secret_key=base.SECRET_KEY, secure=False)
-    try:
-        response = client.get_object("spells", "spells.json")
-        if response:
-            spells_json = json.loads(response.read())
-            print(spells_json.iter())
-            for k, v in spells_json.iter():
-                if k == 'index':
-                    print(f'k: {k}')
-                    spell = base.get_api_spell_index(v)
-                    # get spell from index and save as {index-name}.json
-        return 'spells.json'
-    except Exception as err:
+    response = client.get_object("spells", "spells.json")
+    if response:
+        spells_json = json.loads(response.read())['results']
+        for cached_spell in spells_json:
+            index = cached_spell['index']
+            print(f'getting index: {index}...')
+            try:
+                spell = get_index_spell(index)
+                print('got cached spell: {index}')
+            except:
+                api_spell = base.get_api_spell_index(index)
+                save_index_spell("spells", f'indexes/{index}.json', api_spell)
+    else:
         spells = base.get_spells_from_api()
         client.put_object("spells", "spells.json",
                           BytesIO(json.dumps(spells).encode('utf-8')), length=-1, part_size=10*1024*1024)
