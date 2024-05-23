6f5ef498e48c0cffc573d6a22f0a83fa168507f5
Vitor Roque <vitor.roquep@gmail.com>
2024-05-22 23:11:31 -0300

    feat: adding trino, minio and mysql

--- mage/spells/transform/main.py
+++ mage/spells/transform/main.py
@@ -14,7 +14,6 @@ from . import components
 from . import range as transform_range
 from . import damage
 from . import grouping
-from multiprocessing import Process
 
 client = Minio('minio1:9000', access_key=base.ACCESS_KEY,
                secret_key=base.SECRET_KEY, secure=False)
@@ -25,8 +24,11 @@ minio = s3fs.S3FileSystem(key=base.ACCESS_KEY,
                           client_kwargs={
                               'endpoint_url': 'http://minio1:9000'
                           })
+
+
 def get_spell_by_index(spell: str):
     client.get_object("spells", f'indexes/{spell}.json')
+
 
 def transform_data(obj: Object) -> dict:
     obj_content = client.get_object("spells", obj.object_name)
@@ -44,20 +46,20 @@ def main():
 
     objects = client.list_objects("spells", prefix="indexes", recursive=True)
     processed_spells = list(map(transform_data, objects))
+    grouped = grouping.group_by_level(processed_spells)
     # TODO: group by itertools and save the group in your
     #       respective processed level directory
     # ==================================================
-    #df = pd.DataFrame(processed_spells)
-    #table = Table.from_pandas(df)
-    #pq.write_to_dataset(
+    # df = pd.DataFrame(processed_spells)
+    # table = Table.from_pandas(df)
+    # pq.write_to_dataset(
     #    table,
     #    "s3://spells/processed",
     #    filesystem=minio,
     #    use_dictionary=True,
     #    compression="snappy",
     #    version="2.6"
-    #)
-
+    # )
 
 
 if __name__ == '__main__':
